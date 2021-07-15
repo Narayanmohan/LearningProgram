18db06dbbc2a34e3449106d38fce8b43c3b427cc
Narayanmohan <mohan@cplusplus.us-central1-a.c.cplusplus1.internal>
2021-07-15 04:16:15 +0000

    *P89.c*

---
+++ p89.c
@@ -0,0 +1,86 @@
+/* Program No-P89.c
+dt-7-14-2021
+Concept: Matrix
+Program Name:Find the sum of 2 Matrixes */
+
+int i,j,a[3][3],b[3][3],c[3][3];
+
+i=0;
+
+while(i<3)
+{
+    j=0;
+
+    while(j<3)
+    {
+        
+        scanf("%d",&a[i][j]);
+
+        j++;    
+    
+    }
+
+    i++;
+
+}
+
+    i=0;
+    while(i<3)
+    {
+    
+        j=0;
+        while(j<3)
+        {
+            scanf("%d",&b[i][j]);
+
+            j++;
+        
+        }
+    i++;
+    
+    }
+
+    
+    i=0;
+
+    while(i<3)
+    {
+        j=0;
+
+        while(j<3)
+        {
+        
+            c[i][j] = a[i][j]+ b[i][j];
+
+            j++;
+        
+        }
+    i++;
+        
+    
+    }
+
+
+i=0;
+
+while(i<3)
+{
+
+    j=0;
+
+    while(j<3)
+    {
+    
+     printf("%d",c[i][j]);
+    
+     j++;    
+    
+    
+    }
+
+i++;
+
+}
+
+
+}
