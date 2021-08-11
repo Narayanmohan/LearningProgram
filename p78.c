21198664ad99c2e292f49db1581f20247993ccfd
Narayanmohan <rsa-key-20210804@cplusplus.us-central1-a.c.cplusplus1.internal>
2021-08-11 01:49:37 +0000

    Pushed the p78.c to p82.c

---
+++ p78.c
@@ -0,0 +1,27 @@
+/*Program No- p78.c
+dt-8-10-2021
+Concept: Looping while
+Next Program: Print the * [ Asterisk ] 6 times in the Loop 
+-----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+
+int ctr;
+ctr =1;
+
+while(ctr<6)
+{
+
+    printf("*");
+    
+    ctr = ctr+1;
+    
+
+
+}
+
+
+
+}
