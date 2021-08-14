79b49f616699df99a1befee4aaafbdb6709cc8a9
Narayanmohan <rsa-key-20210804@cplusplus.us-central1-a.c.cplusplus1.internal>
2021-08-14 14:57:49 +0000

    pushing the 83-84-85-86.c

---
+++ p86.c
@@ -0,0 +1,36 @@
+/*Program No- p86.c
+dt-8-14-2021
+Concept: Array
+Next Program: Avg of 10 Numbers using an ARRAY
+------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+int i,sum, a[10];
+float avg;
+sum=0;
+avg=0;
+
+i=0;
+while(i<10)
+{
+
+    scanf("%d",&a[i]);
+    i++;
+
+} 
+
+i=0;
+while(i<10)
+{
+    sum= sum+a[i];
+    i++;
+
+
+}
+
+avg = (sum/i);
+printf("%f",avg); 
+
+
+}
