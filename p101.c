3c6d7b6185eb6afb3868f26c3f4a11b0337b4d40
Narayanmohan <rsa-key-20210804@cplusplus.us-central1-a.c.cplusplus1.internal>
2021-08-15 21:51:31 +0000

    git

--- p101.c
+++ p101.c
@@ -1,30 +1,82 @@
+/*Program No-p101.c
+dt 8-15-2021
+Concept: Array
+Next Program: Read 10 numbers and find the second biggest Numbers
+------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
-int i;
-char a[100];
+
+int i,fbig,sbp,a[10],sp, fsmall,fbp,fsp,sbig;
 
 i=0;
-scanf("%c",&a[i]);
-while(a[i]!='$')
+while(i<=9)
+{
+scanf("%d",&a[i]);
+
+i++;
+
+}
+fbig =a[0];
+i=0;
+while(i<=9)
+{
+ if(fbig <a[i])
+ {
+ 
+  fbp =i;
+  i++;
+ }
+
+else
 {
 
 i++;
-scanf("%c",&a[i]);
-
 
 }
+
+}
+
+fsmall =a[0];
 
 i=0;
-while(a[i]!='$')
+while(i<=9)
 {
- printf("%c",a[i]);
+ if(fsmall = a[i])
+ {
+ 
+  fsp=i;
+  i++;
+  
+ }
+ else
+ {
+  i++;
+ }
+
+}
+
+sbig = fsmall;
+i=0;
+
+while(i<=9)
+{
+ 
+ if(sbig<a[i] && a[i]!=fbig)
+ {
+ 
+  sbig = a[i];
+  sbp =i;
+  i++;
+ }
+ else
+ {
+
  i++;
-
+ }
+ 
 }
 
+printf("%d",sbig);
 
 }
-
-
-
---
+++ p102.c
@@ -0,0 +1,112 @@
+/*program No p102.c
+dt 8-15-2021
+Concept:Array
+Next program: Read 10 numbers and Interchange the 2nd biggest Number and Second Smallest Numbers
+-------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+int i,fbig,sbig,fbp,sbp,fsmall,ssmall,fsp,ssp,a[10],swap;
+
+
+i=0;
+while(i<=9)
+{
+scanf("%d",&a[i]);
+i++;
+}
+fbig=a[0];
+i=0;
+while(i<=9)
+{
+
+if(fbig<a[i])
+{
+fbig = a[i];
+fbp =i;
+i++;
+
+}
+else
+{
+i++;
+}
+
+}
+
+fsmall = a[0];
+i=0;
+while(i<=9)
+{
+ if(fsmall >a[i])
+ {
+  
+  fsmall = a[i];
+  fsp=i;
+  i++;
+   
+ 
+ }
+else
+{
+
+ i++;
+}
+
+}
+
+sbig = fsmall;
+i=0;
+while(i<=9)
+{
+
+  if(sbig <a[i] && a[i]!=fbig)
+  {
+  
+    sbp =i;
+    i++;
+  
+  }
+else
+{
+ i++;
+
+}
+
+}
+
+ssmall= fbig;
+i=0;
+while(i<=9)
+{
+    if(ssmall >a[i] && a[i]!=fsmall)
+    {
+      ssmall = a[i];
+      ssp=i;
+      i++;
+    
+    }
+    else
+    {
+        i++;
+    
+    }
+
+
+}
+
+swap = a[sbp];
+a[sbp] = a[ssp];
+a[ssp] = swap;
+
+i=0;
+
+while(i<=9)
+{
+ printf("%d",a[i]);
+ 
+ i++;
+
+}
+
+}
