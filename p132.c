54cd0066ccf292ed3d6e17979a517de55477976c
Mohanraj <mohaninbox@live.com>
2021-10-12 02:08:53 +0000

    print the number from 1000 to 1

---
+++ p132.c
@@ -0,0 +1,24 @@
+/*program No-p132.c
+dt-10-11-2021
+Concept-For loop
+Next program-Print the number from 1000 to 1
+
+-----------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+int i,j;
+i=1000;
+j=1;
+
+for(i=1000; i>=j;i--)
+{
+
+    printf("%d",i);
+} 
+
+
+}
+
+
