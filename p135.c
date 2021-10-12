3445fc3450e8957499e75764f5be63ebe956cd9e
Mohanraj <mohaninbox@live.com>
2021-10-12 03:40:34 +0000

    Print between two limit

---
+++ p135.c
@@ -0,0 +1,22 @@
+/*Program No p135.c
+dt- 10-11-2021
+Concept- For Loop
+Next Program - Read Two Numbers and print the Numbers between two limits
+------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+int i,j,Firstlimit, Seclimit;
+
+scanf("%d",&Firstlimit);
+scanf("%d",&Seclimit);
+
+     for(i=Firstlimit; i<=Seclimit;i++)
+     {
+             
+         printf("\n print the number between two limit = %d ",i);
+     }
+
+
+}
