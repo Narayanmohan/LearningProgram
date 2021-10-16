597a10ca9684d8e4a1ef7f9e6ba34ef2152d6a99
Mohanraj <mohaninbox@live.com>
2021-10-16 20:04:24 +0000

    Read 10 numbers Find the Mean of 10 numberss in the given array

---
+++ p148.c
@@ -0,0 +1,35 @@
+/*program No - p148.c
+dt-10-16-2021
+Loop Concept- For Loop
+Concept- Array in For Loop
+Next Program- Read 10 numbers and Find the Mean of 10 numbers in the given array
+------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+int i,sum,a[10];
+float avg;
+sum=0;
+avg=0;
+
+
+for(i=0;i<10;i++)
+{
+scanf("%d", &a[i]);
+}
+
+for(i=0;i<10;i++)
+{
+sum= sum+a[i];
+}
+
+avg= (sum/i);
+
+printf("%f",avg);
+
+
+
+
+
+}
