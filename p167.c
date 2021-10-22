00b0edde9a62d1769476f6800dc002a0b66fc410
Mohanraj <mohaninbox@live.com>
2021-10-22 01:29:05 +0000

    Read and display a word until 'space' is given

---
+++ p167.c
@@ -0,0 +1,26 @@
+/*Program p167.c
+dt-10-21-2021
+Loop Concept- For Loop
+Concept-Array in For Loop
+Next Program-> Read and Display a word until 'space' is given
+---------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+int i;
+char a[100];
+    
+for(i=0,scanf("%c",&a[i]); a[i]!=' '; i++,scanf("%c",&a[i]));
+
+
+for(i=0;a[i]!=' ';i++)
+{
+
+    printf("%c",a[i]);
+    
+}
+
+
+
+}
