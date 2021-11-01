785e1abcffb4b5e3af91afffba74bc5e34ca071c
Mohanraj <mohaninbox@live.com>
2021-11-01 02:35:06 +0000

    For Loop-getchar()/putchar()-Read and Display a line until '\n' is given

---
+++ p170.c
@@ -0,0 +1,24 @@
+/*Program No-p170.c
+dt-10-31-2021
+Concept-Char Array
+Loop-For Loop
+Next Program-Read and Display a line until '\n' is given
+---------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+int i;
+char a[100];
+
+for(i=0;(a[i]=getchar())!='\n';i++);
+
+a[i]='\0';
+
+for(i=0;a[i];i++)
+{
+
+    putchar(a[i]);
+
+}
+
+}
