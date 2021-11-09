34c423a45af6ecff5b786db33d78143608924c42
Mohanraj <mohaninbox@live.com>
2021-11-09 23:47:08 +0000

    For Loop-Read text until '$' is given and copy the Last 'Nth' characters

--- p178.c
+++ p178.c
@@ -2,6 +2,18 @@
 dt-10-26-2021
 Concept-For Loop
 Next program-Read text until '$' is given and copy the last 'n' characters
+------------------------------------------------------------------------------------------
+Example:
+Input -> I LOVE INDIA
+
+
+CTR = 12
+N = 5;
+
+OUTPUT - INDIA
+
+I = 12-5 = 7
+
 -----------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
@@ -29,7 +41,7 @@ for(i=0;a[i];i++)
 
 scanf("%d",&num);
 
-for(i=num,j=0; i<=ctr; i++,j++)
+for(i=ctr-num,j=0; a[i];  i++,j++)
 {
 
     b[j] = a[i];
