9ac7ccd99cfe9dfe5da5a8858ed2381b4bf8b777
Mohanraj <mohaninbox@live.com>
2021-11-10 22:23:23 +0000

    For Loop-Print Starting Position of each line

---
+++ p187.c
@@ -0,0 +1,53 @@
+/*Program No-p187.c
+dt-11-10-2021
+Concept- Char Array
+Loop- For Loop
+Next Program-Print Starting Position of each line
+---------------------------------------------------------------------  
+Example:
+
+INPUT -> [o][n][e][\n][t][w][o][\n][l][r]
+
+
+OUTPUT ->[0][4][8][-1][][]
+          0  1  2  3  4 5
+ 
+
+---------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+int i,j,po;
+char a[100];
+int b[100];
+po=0;
+
+for(i=0;(a[i]=getchar())!='$';i++);
+
+a[i]='\0';
+b[j]=i;
+
+for(i=0,j=0;a[i];i++)
+{
+
+    if(a[i]=='\n')
+    {
+        po=i+1;
+        b[j]=po;
+        j++;
+    }
+
+
+}
+
+b[j]=-1;
+for(j=0;b[j]!=-1;j++)
+{
+
+    printf("%d",b[j]);
+
+}
+
+
+}
