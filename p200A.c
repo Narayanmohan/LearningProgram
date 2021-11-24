80ad951d423b658788a51b1694469c3cdf492ee4
Mohanraj <mohaninbox@live.com>
2021-11-24 21:00:11 +0000

    Structure-Biggest of three numbers using Nested ternary operator,and using structure

---
+++ p200A.c
@@ -0,0 +1,28 @@
+/*program No-p200A.c
+dt-11-24-2021
+Concept-Structure
+Next Program-Biggest of three numbers using Nested ternary opearators using structure
+---------------------------------------------------------------------------------------------
+#include<stdio.h>
+int main()
+{
+    struct big3
+    
+    {
+    
+        int a,b,c,b3;
+
+    }biggest3;
+
+scanf("%d%d%d%d",&biggest3.a,&biggest3.b,&biggest3.c);
+
+biggest3.b3= biggest3.a>biggest3.b?(biggest.a >biggest3.c ?biggest3.a:biggest3.c):biggest3.b>biggest3.c?biggest3.b:biggest3.c;
+
+
+printf("Biggest of three is = %d",biggest3.b3);
+
+
+
+
+
+} 
