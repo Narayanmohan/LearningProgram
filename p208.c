a9e97b50ef99d83cc5cdfa3ab18250845978fcfc
Mohanraj <mohaninbox@live.com>
2021-11-26 16:17:36 +0000

    Typedef- Read and display two complex numbers

---
+++ p208.c
@@ -0,0 +1,29 @@
+/*program No p208.c
+dt-11-26-2021
+Concept-Typedef
+Next Program - Read and Display two complex Numbers
+-------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+typedef struct complex
+{
+    int a;
+    int b;
+    
+}COMPLEX;
+
+COMPLEX comp1,comp2;
+
+
+scanf("%d%d%d%d",&comp1.a,&comp1.b,&comp2.a,&comp2.b);
+
+
+printf("\n%d+i%d",comp1.a,comp1.b);
+
+printf("\n%d+i%d",comp2.a,comp2.b);
+
+
+
+}
