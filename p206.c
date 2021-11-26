c6f61a415803ff474ea581bab686518137d1b201
Mohanraj <mohaninbox@live.com>
2021-11-26 15:47:16 +0000

    Typedef and Struct within  struct-Read and Display bio-data with address and dob using struct within struct & typedef

---
+++ p206.c
@@ -0,0 +1,51 @@
+/*Program No-p206.c
+dt-11-25-2021
+Concept-Typedef with Structure within structure
+Next Program-Read and Display bio-data with address & dob using struct within struct and typedef
+--------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+ typedef struct address
+ {
+ 
+     char street[100];
+     char city[100];
+     int pincode;
+  
+ }Addr;
+ 
+ typedef struct date
+ {
+     int day;
+     int mm;
+     int year;
+  
+ }Dob;
+
+typdef struct biodata
+ {
+     char name[100];
+     int age;
+     float salary;
+     Addr a;
+     Dob d;
+  
+ }Bio;
+
+Bio b;
+
+scanf("%s%s%d%d%d%d%s%d%f",b.a.street,b.a.city,&b.a.pincode,&b.d.day,&b.d.mm,&b.d.year,b.name,&b.age,&b.salary);
+
+printf("Street:%s\n",b.a.street);
+printf("City:%s\n",b.a.city);
+printf("Pincode:%s\n",b.a.pincode);
+printf("Day:%d\n",b.d.day);
+printf("Month%d\n",b.d.mm);
+printf("Year:%d\n",b.d.year);
+printf("Name:%s\n",b.name);
+printf("Age:%d\n",b.age);
+printf("Salary:%d\n",b.salary);
+
+}
