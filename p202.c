b4b4e3241f6dd77c9709e0e2c55893d4aba29591
Mohanraj <mohaninbox@live.com>
2021-11-28 14:55:26 +0000

    Read and display bio data (name,age,salary,address,dob)using struct within struct - dob-data,month, year

--- p202.c
+++ p202.c
@@ -40,7 +40,10 @@ int main()
 scanf("%s%d%f%d%d%d%s%s%d",bio.name,&bio.age,&bio.salary,&bio.dob.day,&bio.dob.mm,&bio.dob.year,bio.addrs.street,bio.addrs.city,&bio.addrs.pincode);
 
 
-printf("Name:%s\n",bio.name);
+printf("\n\t%s\n\t%d\n\t%f\n\t%d\n\t%d\n\t%d\n\t%s\n\t%s\n\t%d",bio.name,bio.age,bio.salary,bio.dob.day,bio.dob.mm,bio.dob.year,bio.addrs.street,bio.addrs.city,bio.addrs.pincode);
+
+
+/*printf("Name:%s\n",bio.name);
 printf("Age:%d\n",bio.age);
 printf("Salary:%f\n",bio.salary);
 printf("Day:%d\n",bio.dob.day);
@@ -48,6 +51,6 @@ printf("Month:%d\n",bio.dob.mm);
 printf("Year:%d\n",bio.dob.year);
 printf("Street:%s\n",bio.addrs.street);
 printf("City:%s\n",bio.addrs.city);
-printf("Pincode:%d\n",bio.addrs.pincode); 
+printf("Pincode:%d\n",bio.addrs.pincode); */
 
 }
