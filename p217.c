4bed4ddd6565d4755d596d5605c7b18bff9e1c2c
mohanraj <mohaninbox@live.com>
2021-12-15 20:00:33 +0000

    File Handling-Read Char fromm keyboard & Write it to the file until '$' is given

---
+++ p217.c
@@ -0,0 +1,38 @@
+/*Program No-p217.c
+dt-12-15-2021
+Concept-File Handling
+Next Program-Read char from keyboard and write it to the file until '$' is given
+-------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+ char a;
+ FILE *fp;
+ 
+ scanf("%c",&a);
+ 
+ fp=fopen("Readcha.txt","w");
+ 
+      if(fp==NULL)
+      {
+      
+         printf("File Opening Error");
+         return 0;
+         
+      }
+      
+ while(a!='$')
+ {
+ 
+     fprintf(fp,"%c",a);
+     
+     scanf("%c",&a);
+  
+ }     
+ 
+
+ fclose(fp);     
+      
+ }
+
