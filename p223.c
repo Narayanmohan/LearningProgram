a0c86e33e38f94248ca1edaa8f1639c4c47ba8ec
mohanraj <mohaninbox@live.com>
2021-12-15 23:39:05 +0000

    CommandLineArgument-Read biodata from file till end and display on the screen using command line Argument

---
+++ p223.c
@@ -0,0 +1,52 @@
+/*Program No-p223.c
+dt-12-15-2021
+Concept-File Handling
+Next Program-Read bio data from file till end and display on the screen using command line Argument
+--------------------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+
+typedef struct bio
+{
+
+char name[100];
+int age;
+float salary;
+}Bio;
+
+Bio b;
+
+FILE *fp;
+
+    if(argc<2)
+    {
+
+    printf("Invalid Argument");
+    return 0;
+
+    }
+
+fp =fopen("Bio5.txt","r");
+
+    if(fp== NULL)
+     {
+ 
+     printf("File Opening Error");
+     return 0;
+     }
+ 
+while(feof(fp)==0)
+{
+ fscanf(fp,"%s%d%f",b.name,&b.age,&b.salary);
+ 
+ printf("\n%s\n%d\n%f",b.name,b.age,b.salary);
+
+
+}
+
+fclose(fp);
+
+
+
+}
