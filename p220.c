ed7d2a93de009b15f7c7e0eef2e77238201b3ece
mohanraj <mohaninbox@live.com>
2021-12-15 21:53:25 +0000

    NewConcept-Command Line Argument-Read biodata from keyboard and write it to a file using command line Argument

---
+++ p220.c
@@ -0,0 +1,65 @@
+/*program No-p220.c
+dt-12-15-2021
+Concept-Command Line Argument
+Next Program-Read biodata from keyboard and write it to the file using command line Argument
+---------------------------------------------------------------------------------------------------
+Introduction:
+ - We pass the value before the program begins execute
+ 
+
+int main(int argc , char *argv[])
+
+1-argc -> count of argument
+
+2-argv[0] ->p220.c (ie. file name)
+3-argv[1] ->biodata.txt
+----------------------------------------------------------------------------------------------------
+
+When Execute follow the below steps:
+
+ -  cc p220.c
+ - ./a.out Bio2.txt
+ - joe p220.c
+
+---------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main(int argc , char *argv[])
+{
+
+    typedef struct biodata
+    {
+    
+        char name[100];
+        int age;
+        float salary;
+        
+    }Bio;
+    
+Bio b;
+
+
+FILE *fp;
+
+if(argc < 2)
+{
+    printf("Invalid Argument");
+    return 0;
+    
+}
+
+fp = fopen(argv[1],"w");
+
+ if(fp==NULL)
+ {
+ 
+  printf("File Opening Error");
+  return 0;    
+ 
+ }
+
+scanf("%s%d%f", b.name,&b.age,&b.salary);
+
+fprintf(fp,"%s%d%f",b.name,b.age,b.salary);
+fclose(fp);
+
+}
