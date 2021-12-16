6072c54b1e2aff584305578b74c7b68fdc8295a1
mohanraj <mohaninbox@live.com>
2021-12-16 21:01:35 +0000

    CommandLineArgument-FileHandling-Read character from the file and Display the decrypted file on the screen

---
+++ p230.c
@@ -0,0 +1,67 @@
+/*Program No-p230.c
+dt-12-16-2021
+Concept-FileHandling
+Concept-CommandLineArgument
+NextProgram-Read character from the file and Display the decrypted file on the screen
+------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main(int argc , char *argv[])
+{
+char ch;
+FILE *fp1;
+FILE *fp2;
+
+
+
+if(argc < 3)
+{
+
+    printf("Invalid Argument");
+    return 0;
+}
+
+
+fp1=fopen(argv[1],"r");
+fp2=fopen(argv[2],"w");
+
+
+if(fp1==NULL ||fp2==NULL)
+{
+
+    printf("File Opening Error");
+    return 0;
+
+}
+
+ch = getc(fp1);
+while(ch!=EOF)
+{
+
+    ch = ch-1;
+    putc(ch,fp2);
+    ch = getc(fp1);
+
+
+}
+
+ch=getc(fp2);
+
+while(ch!=EOF)
+{
+
+    putchar(ch);
+    ch=getc(fp2);
+
+}
+
+fclose(fp1);
+fclose(fp2);
+
+
+
+
+
+
+
+
+}
