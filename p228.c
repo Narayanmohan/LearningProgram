29b2c3a3ca99d80fd53f709b6fcbdf8b5017ec6b
mohanraj <mohaninbox@live.com>
2021-12-16 17:51:52 +0000

    CommandLineArgument-FileHandling-Convert uppercase to lowercase using command line argument and display on the screen

---
+++ p228.c
@@ -0,0 +1,61 @@
+/*Program No-p228.c
+dt-12-16-2021
+Concept-CommandLineArgument
+Concept-FileHandling
+NextProgram-Convert Uppercase to Lowercase using CommandLindArgument and Display on the Screen
+--------------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main(int argc, char *argv[])
+{
+
+char ch;
+FILE *fp1;
+FILE *fp2;
+
+if(argc< 3)
+{
+    printf("Invalid Argument");
+    return 0;
+}
+
+fp1 = fopen(argv[1],"r");
+fp2=  fopen(argv[2],"w");
+
+
+if(fp1==NULL ||fp2==NULL)
+{
+
+    printf("File Opening Error");
+    return 0;
+}
+
+
+ch = getc(fp1);
+while(ch!=EOF)
+{
+
+    if(ch>=65 && ch<=90)
+    {
+        ch = ch+32;
+        
+    
+    }
+
+ putc(ch,fp2);
+ ch = getc(fp1);
+}
+
+
+ch = getc(fp2);
+while(ch!=EOF)
+{
+
+    putchar(ch);
+    ch = getc(fp2);
+
+}
+
+fclose(fp1);
+fclose(fp2);
+
+}
