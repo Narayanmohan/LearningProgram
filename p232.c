418b68de2f06cb4075aa6fc0a05c8a6ba9974ca7
mohanraj <mohaninbox@live.com>
2021-12-17 13:36:21 +0000

    CommandLindArgument-FileHandling-Read characters from file until EOF and Display on the screen using char array

---
+++ p232.c
@@ -0,0 +1,63 @@
+/*Program No-p232.c
+dt-12-16-2021
+Concept-CommandLineArgument
+Concept-File Handling
+Concept-Using CharArray
+------------------------------------------------------------------------------------------------------
+Note:
+
+ putchar function can be used for to display on the screen.
+
+
+
+------------------------------------------------------------------------------------------------------*/
+
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+
+ int i;
+ char a[100];
+ FILE *fp2;
+
+
+if(argc< 2)
+{
+
+ printf("invaild Argument");
+ return 0;
+
+}
+
+
+fp2 = fopen(argv[1],"r");
+
+if(fp2==NULL)
+{
+
+ printf("File Opening Error");
+ return 0;
+
+}
+
+
+
+for(i=0;(a[i]=getc(fp2)!=EOF);i++);
+
+a[i]='\0';
+
+
+
+
+for(i=0; a[i]!='\0';i++)
+{
+
+ putchar(a[i]);
+
+}
+
+fclose(fp2);
+
+}
+ 
+ 
