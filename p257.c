44b2d9f42669fa300fb30ea8750f1c05f8a4437f
mohanraj <mohaninbox@live.com>
2022-02-17 21:46:40 +0000

    FileHandling-Display Nthrecord along with payslip

---
+++ p257.c
@@ -0,0 +1,101 @@
+/*Program No-p257.c
+dt-2-16-2022
+Concept-File Handling
+Next Program-Display the Nth record along with payslip
+--------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main(int argc,char *argv[])
+{
+
+int i,nthrecord,offset,offset1,ctr;
+float hra;
+float da;
+float pf;
+float netsalary;
+char payslip;
+
+
+union Biodata
+{
+
+    struct bio
+    {
+    
+        char name[100];
+        int age;
+        float salary;
+         
+    }b;
+    char ch[sizeof(struct bio)];
+
+};
+
+
+union Biodata m;
+
+FILE *fp;
+FILE *fp1;
+
+if(argc<3)
+{
+
+    printf("Invalid Argument");
+    return 0;
+
+}
+
+fp=fopen(argv[1],"r");
+fp1=fopen(argv[2],"r");
+
+if(fp==NULL || fp1==NULL)
+{
+
+    printf("File Opening Error");
+    return 0;
+
+}
+
+printf("Enter Nthrecord");
+
+scanf("%d",&nthrecord);
+
+offset = (nthrecord-1)*(sizeof(struct bio));
+
+fseek(fp,offset,0);
+
+for(i=0;i<(sizeof(struct bio));i++)
+{
+m.ch[i]=getc(fp);
+}
+
+
+printf("Name =%s\n Age=%d\n Salary=%f\n",m.b.name,m.b.age,m.b.salary);
+
+
+hra=m.b.salary*0.1;
+da=m.b.salary*0.5;
+pf=m.b.salary*0.2;
+
+netsalary = m.b.salary *((hra+da)-(pf));
+
+ctr=0;
+
+while(feof(fp1)==0)
+{
+
+ctr =ctr+1;
+
+fscanf(fp1,"%f%f%f",&hra,&da,&pf);
+
+
+   if(ctr==nthrecord)
+   {
+   
+       printf("HRA=%f\n DA=%f\n,PF=%f\n NETSALARY=%f\n",hra,da,pf,netsalary);
+  
+   }
+}
+fclose(fp);
+fclose(fp1);
+
+}
