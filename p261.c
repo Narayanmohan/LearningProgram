74fcba083d8be269e7ddbe393e5de81ed22d1d1c
mohanraj <mohaninbox@live.com>
2022-02-22 18:29:12 +0000

    Function-Find the biggest of 3 numbers using Function and Ternary Operators

---
+++ p261.c
@@ -0,0 +1,37 @@
+/*Program No-p261.c
+dt-2-22-2022
+Concept-Function
+Next Program-Find the biggest of Three numbers using Function and Ternary Operators
+------------------------------------------------------------------------------------*/
+#include<stdio.h>
+
+int big3(int a,int b, int c);
+
+int main()
+{
+
+int fn,sn,tn,r;
+
+printf("---Enter value for fn,sn,tn---\n");
+
+scanf("%d%d%d",&fn,&sn,&tn);
+
+r = big3(fn,sn,tn);
+
+printf("Biggest of Three Number is = %d\n",r);
+
+}
+
+int big3(int a, int b, int c)
+{
+
+    int t,s;
+    
+    t= a>b?a:b;
+    
+    s = t>c?t:c;
+    
+    return s;
+    
+
+}
