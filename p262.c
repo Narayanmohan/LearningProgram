4ad4f004567610b8785e0fa5c9ef109008c9ed08
mohanraj <mohaninbox@live.com>
2022-02-23 14:05:23 +0000

    Function-Find the Area of Circle using Function

---
+++ p262.c
@@ -0,0 +1,36 @@
+/*Program No-p262.c
+dt-2-23-2022
+Concept-Function
+Next program-Find the Area of circle using Function
+------------------------------------------------------------*/
+#include<stdio.h>
+float aoc(float num1);
+int main()
+{
+
+    int r;
+    float areaofcir;
+    
+ 
+    printf("----Enter Value For Radious----\n");
+    
+    scanf("%d",&r);
+   
+    areaofcir = 3.14 *r*r;
+    
+    printf("Area of Circle = %f",areaofcir);
+
+
+
+}
+
+
+float aoc(float num1)
+{
+
+    float t;
+    
+    t=3.14 *num1 *num1;
+    return t;
+
+}
