855fb809a24cc54e05bc0622a65849ba2e33bef0
mohanraj <mohaninbox@live.com>
2022-02-27 15:26:17 +0000

    P4-Area of Circle

--- p4.c
+++ p4.c
@@ -1,20 +1,22 @@
-/*Program 4.c
+/*Program p4.c
 dt 4-13-2021
-Area of circle */
+Next program-Area of circle */
 #include<stdio.h>
 int main()
 {
   float Area;
+
   float Pi;
+
   int r;
   
   Pi = 3.14;
+
   r =7;
   
   Area = Pi*r*r;
   
   printf("Area of Circle = %f",Area);
-
 
 
 
