473e63e4f7445af2bbdb87235284ba260d04b05c
mohanraj <mohaninbox@live.com>
2022-02-27 15:42:30 +0000

    P9-Sum of Two Numbers Using Scanf Function

--- p9.c
+++ p9.c
@@ -1,19 +1,16 @@
-/* PROGRAM P9.C
+/* PROGRAM p9.C
 DT-4-14-2021
-SUM OF TWO NUMBERS USING SCANF FUNCTION */
-
-
+Next Program-SUM OF TWO NUMBERS USING SCANF FUNCTION
+----------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
-  int a, b,c;
+   int a, b,c;
   
-  scanf("%d%d",&a,&b);
+   scanf("%d%d",&a,&b);
     
-  c = a+b;
+   c = a+b;
   
 printf("sum of two numbers are =%d" ,c);
-
-
 
 }
