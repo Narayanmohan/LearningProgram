980ef829661fb883e4cd7c70996193208c37e998
mohanraj <mohaninbox@live.com>
2022-02-27 15:24:45 +0000

    P3-Product of Two Real Numbers

--- p3.c
+++ p3.c
@@ -1,14 +1,17 @@
 /*Program p3.c
 dt 4-13-2021
-Product of two real numbers */
+Next Program-Product of two real numbers */
 
 #include<stdio.h>
 int main()
 {
+
 float a, b,c;
 
     a= 17;
+
     b=4.7;
+
     c = a*b;
 
 printf("Product of two real numbers =%f",c);
