edcb8905f1386197357380f72d51f4e9cca29ce9
mohanraj <mohaninbox@live.com>
2022-02-27 15:39:50 +0000

    P8-Fahrenheit to Celcious

--- p8.c
+++ p8.c
@@ -1,24 +1,23 @@
 /*program P8.C
 DT-4-14-2021
 C= (5.0/9.0)*(F-32)
-Fahrenheit to Celcious */
+Next Program -Fahrenheit to Celcious
+--------------------------------------------------------------------*/
 
 #include<stdio.h>
 int main()
 {
 
 float Fartocel;
+
 int f;
+
 f = 7;
 
 
 Fartocel = (5.0/9.0)*(f-32);
 
 printf("Fartocel= %f ",Fartocel);
-
-
-
-
 
 
 
