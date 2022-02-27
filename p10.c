5aed6ce1942d3b049cf4f1735012883889ee1f66
mohanraj <mohaninbox@live.com>
2022-02-27 16:32:21 +0000

    P10-Product of two Real Numbers using Scanf Function

--- p10.c
+++ p10.c
@@ -1,14 +1,15 @@
 /* Program p10.c
 dt - 4-14-2021
-PRODUCT OF TWO REAL NUMBERS USING SCANF FUNCTIONS */
-
+Next Program-PRODUCT OF TWO REAL NUMBERS USING SCANF FUNCTIONS
+-----------------------------------------------------------------------*/
 #include<stdio.h>
+
 int main()
 {
 
     float Product,a,b;
 
-    scanf("%f%f",&a,&b);
+        scanf("%f%f",&a,&b);
 
         Product = a*b;
         
