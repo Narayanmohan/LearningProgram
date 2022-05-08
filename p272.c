49cc4abc104975e7d02f02c97361ac23cb478fb0
Narayanmohan <mohaninbox@live.com>
2022-05-08 03:11:58 +0000

    P272.c-Function-Find the Biggest of Ten numbers

---
+++ p272.c
@@ -0,0 +1,50 @@
+/*program No-p272.c
+dt-5-6-2022
+Concept-Function
+Next Program-Find the biggest of ten numbers 
+--------------------------------------------------------*/
+#include<stdio.h>
+void Read(int a[]);
+int Bigten(int b[]);
+int main()
+{
+
+int a[10],s;
+
+Read(a);  
+
+s=Bigten(a); 
+
+printf("%d",s);
+
+}
+
+
+void Read(int a[])
+{
+    int i;
+   
+    for(i=0;i<10;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+
+}
+
+int Bigten(int b[])
+{
+
+    int j,Biggest;
+    
+    Biggest=b[0];
+    
+     if(Biggest<b[j])
+     {
+        Biggest=b[j];
+     
+     }
+
+return Biggest;
+    
+ }
