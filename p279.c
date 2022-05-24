f1a8be5ac55ead42840e21faaa3b19192c1ca16e
Narayanmohan <mohaninbox@live.com>
2022-05-24 01:24:05 +0000

    P279-Function-Read and Display text until $ is given

---
+++ p279.c
@@ -0,0 +1,51 @@
+/*Program No-p279.c
+dt-5-22-2022
+Concept-Function
+Next Program-Read and Display text until $ is given
+-------------------------------------------------------*/
+#include<stdio.h>
+void readtext(char a[]);
+void Display(char b[]);
+int main()
+{
+ 
+    char a[100];
+
+    char b[100];   
+    
+     
+    readtext(a);
+
+    Display(a);
+
+}
+
+void readtext(char a[100])
+{
+
+    int i;
+    
+
+    for(i=0;(a[i]=getchar())!='$';i++);
+ 
+
+    a[i]='\0';
+      
+
+}
+
+
+void Display(char b[100])
+{
+
+
+   int j;
+   
+  for(j=0;b[j]; j++)
+  { 
+   
+       putchar(b[j]);
+  }
+   
+
+}
