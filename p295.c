ea5c3deb39a9ef9b6e7890861b613d020eb41800
Narayanmohan <mohaninbox@live.com>
2022-06-26 14:40:05 +0000

    P295-Function-Count the No of times the given pattern exist

--- p295.c
+++ p295.c
@@ -2,30 +2,25 @@
 dt-6-25-2022
 Concept-Function
 Next Program-Count the number of times the given pattern exits
--------------------------------------------------------------------------------*/
+----------------------------------------------------------------*/
 #include<stdio.h>
+
 void read(char a[100]);
-int noofTimesPatternExist(char a[100], char b[100]);
-int count;
+int TimesPatternExist(char a[100], char b[100]);
 int main()
 {
-    char a[100];
-    
-    char b[100];
-    
-    int total;
-    
-    read(a);
-    
-    read(b);    
-    
-    total = noofTimesPatternExist(a,b);
 
+    char a[100];
+    char b[100];
+    int total;
+    read(a);
+    read(b);    
+  
+    total = TimesPatternExist(a,b);
+  
     printf("Number of Times the Pattern Exist is = %d",total);
 
-
 }
-
 
 void read(char a[])
 {
@@ -36,30 +31,36 @@ void read(char a[])
     
     a[i]='\0';
 
-
 }
 
 
-int noofTimesPatternExist(char a[],char b[])
+int TimesPatternExist(char a[],char b[])
 {
 
-    int i,j,k;
+      int i,j,k,count;
     
-        for(i=0,count=0;a[i];i++)
-            {
+              
+        for(i=0,count=0;a[i]!='\0';i++)
+             {
+               
+                  for(k=i,j=0; a[k]==b[j] && b[j]!='\0'; k++,j++);
                 
-                for(k=i,j=0;a[k]==b[j]&&b[j]!='\0';k++,j++);
-                
-                
-                    if(b[j]=='\0');
-                    {
-                    
-                        count = count+1;
-                    }
-                
-            
-            }
-    
-        return count;
 
+                      if(b[j]=='\0')
+                      {
+                           
+                         count = count+1;
+                       
+                      }
+                                      
+                //return count;        
+                
+                //printf("The total count is = %d",count);
+
+             }   
+
+return count;
+         
+printf("the total count = %d",count);
+   
 }
