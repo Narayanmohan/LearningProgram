5df674225131195700ef3d75e9e8a7cd88289a76
Narayanmohan <mohaninbox@live.com>
2022-07-16 21:27:30 +0000

    P72-While Loop-Classify the given character is single digit/upper/Lower/Spl.character

--- p72.c
+++ p72.c
@@ -1,6 +1,38 @@
 /*Program No- p72.c
 dt-8-10-2021
 Concept: Looping
-Next Program: Read and Display 10 numbers
----------------------------------------------------------*/
+Next Program:Classify the given character is single digit/Upper or special character until $ is given
+------------------------------------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    char cha;
+    
+    scanf("%c",&cha);
+    
+        while(cha!='$')
+        {
+        
+            if(cha>=65 && cha<=90)
+             
+                  printf("%c is Upper case",cha);
+            
+            else if(cha>=97 && cha <=122)
+                
+                  printf("%c is Lower case",cha);
+                  
+            else if(cha>=48 && cha<=57)
+
+                  printf("%c is Single digit",cha);
+            
+            else
+                 printf("%c is special character",cha);
+                 
+            scanf("%c",&cha);              
+        
+        }
+
+
+
+}
 
