b3c42dfd664009aca80d05a2f15133d61fa6120e
Narayanmohan <mohaninbox@live.com>
2022-07-16 18:18:01 +0000

    P75-While Loop-Avg of 10 Numbers

--- p75.c
+++ p75.c
@@ -1,6 +1,7 @@
 /*program No - P75.c
 dt-8-10-2021
 Concept- While Looping
+
 Next Program: Mean / Avg of 10 Numbers 
 ---------------------------------------------------------------*/
 #include<stdio.h>
