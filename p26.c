9ac076d180e227372c096f2248745363a9330e6f
Narayanmohan <mohaninbox@live.com>
2022-07-16 16:47:08 +0000

    P26-IF Statement-Given Integer number is Two Digit number or Not

--- p26.c
+++ p26.c
@@ -1,13 +1,15 @@
 /* Program No- p26.c
 dt-4-15-2021
-IF CONTROL STRUCTURE
-GIVEN INTEGER NUMBER IS TWO DIGIT NUMBER OR NOT */
+Concept-IF CONTROL STRUCTURE
+Next Program -GIVEN INTEGER NUMBER IS TWO DIGIT NUMBER OR NOT
+---------------------------------------------------------------------------------------*/
 
 #include<stdio.h>
 int main()
 {
 
  int num;
+
  scanf("%d",&num);
  
      if((num>=10) && (num<=99))
