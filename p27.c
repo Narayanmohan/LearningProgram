3cb4785513a85538cad77e7cace205a6b8d4809e
Narayanmohan <mohaninbox@live.com>
2022-07-16 16:48:39 +0000

    P27-IF Statement-Given number is single digit,two dt,three digit or Four Digit

--- p27.c
+++ p27.c
@@ -1,7 +1,8 @@
 /* Program No  p27.c
 dt- 4-15-2021
-IF CONTROL STRUCTURE
-Given integer number is single digit, two digit,three digit or four digit */
+Concept -IF CONTROL STRUCTURE
+Next Program-Given integer number is single digit, two digit,three digit or four digit
+----------------------------------------------------------------------------------------------------*/
 
 #include<stdio.h>
 int main()
@@ -11,18 +12,23 @@ int main()
     
     scanf("%d",&num);
     
+
         if((num>=0) && (num<=9))
             printf("Entered number is Single digit = %d",num);
             
+
         else if((num>=10) && (num<=99))
             printf("Entered number is Two digit = %d", num);
             
+
         else if((num>99) &&(num<=999))
             printf("Entered number is Three digit = %d",num);
             
+
         else if((num>999)&&(num<=9999))
             printf("Entered number is Four digit = %d",num);
             
+
         else
             printf(" Entered number is NOT a valid number = %d",num);
 
