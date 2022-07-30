d25c129680ccc8f94c35e8532bbe8ab918e00379
Narayanmohan <mohaninbox@live.com>
2022-07-30 22:05:51 +0000

    P23-If Statment-Given integer number is even or not

--- p23.c
+++ p23.c
@@ -3,8 +3,7 @@ dt- 4-15-2021
 Concept-If Statement-if control structure
 NOTE-% operator gives remainder of an integer division
 Next Program - Given integer number is Even or Not
-------------------------------------------------------------------------------------------*/
-
+-----------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
