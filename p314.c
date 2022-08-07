5eed140bcd366aabffe3c863bb4d8efc801cea3b
Narayanmohan <mohaninbox@live.com>
2022-08-07 21:25:48 +0000

    P314-Pointers-Product of two real numbers

---
+++ p314.c
@@ -0,0 +1,26 @@
+/*Program No-p314.c
+dt-8-7-2022
+Concept-Pointers
+Next Program-Product of Two Real Numbers
+-----------------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    float a, b,*pa,*pb,*pc;
+    
+        pa =&a;
+    
+        scanf("%f",pa);
+    
+        pb = &b;
+    
+        scanf("%f",pb);
+      
+         
+             *pc= (*pa) * (*pc);
+        
+        printf("%f",*pc);
+    
+
+}
