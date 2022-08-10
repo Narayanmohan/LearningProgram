c12807257b6072fd6226e73daed04a1d0c7d60e1
Narayanmohan <mohaninbox@live.com>
2022-08-10 16:13:49 +0000

    P317a-Pointers with Char Array-Read and Display a text until $ is given

--- p317a.c
+++ p317a.c
@@ -3,29 +3,32 @@ dt-8-9-2022
 Concept-Char Array
 Concept-Pointers with CharArray
 Next Program-Read and display a text until $ is given
----------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------
 Notes:
+
+For First character in the array it stores 8 bytes 108
+For Next character in the array it stores 8 bytes  116...
+
 In the below char a array
-
- a=> |  |   |  |  |  |  |  |  
-
-
-
+      100 108 116
+ a=> |   |   |   |   |   |   |   |  
+       0   1   2   3   4   5   6  	 	
 
 
+char *pa
+
+Herebelow we are assiging the first index[0]th address to the pointer variable.
+pa= a -> Means Array always pointing first index Address by default. 
+
+When comes to Array, we should NOt use & to get Address b'coz it always pointing to the first address of an array ie a[0]th Address =108
 
 
-
-
-
-
---------------------------------------------------------------------*/
+--------------------------------------------------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
     
-    int i;
-  
+      
     char a[100];
 
     char *pa;
