af318c66560178a8c4d5b7b4e201fa8e70dd49e6
Narayanmohan <mohaninbox@live.com>
2022-08-10 16:03:34 +0000

    P317a-Pointers with Char Array-Read and Display a text until $ is given

--- p317a.c
+++ p317a.c
@@ -3,29 +3,50 @@ dt-8-9-2022
 Concept-Char Array
 Concept-Pointers with CharArray
 Next Program-Read and display a text until $ is given
---------------------------------------------------------------*/
+---------------------------------------------------------------------
+Notes:
+In the below char a array
+
+ a=> |  |   |  |  |  |  |  |  
+
+
+
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
     
     int i;
-    
-    char a[100],*pa;
-    
-    i=0;
-        
-    scanf("%c",&a[i]);
-    
-    pa=&a[i];
-    
-        while(*pa!='$')
-        {
-                                    
-             printf("%c",*pa);
-             scanf("%c",pa);
-             
-             i++;                      
-        
-        } 
+  
+    char a[100];
 
-    }
+    char *pa;
+    
+
+      //Read from keyboard character by characters
+     for(pa=a;(*pa=getchar())!='$';pa++);
+      
+      
+        
+      // Display the keyboard input 
+      for(pa=a;*pa!='\0';pa++)
+      {
+      
+       printf("%c",*pa);
+       
+      }
+                                                          
+  
+
+ 
+}  
+  
+    
