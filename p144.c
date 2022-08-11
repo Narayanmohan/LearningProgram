41dc4f0f0c6f4db2f357d3fe1c849d993b54b82f
Narayanmohan <mohaninbox@live.com>
2022-08-11 02:02:34 +0000

    P144-ForLoop-Classify the given character upp,lower,singledigit,splchar until $ given

--- p144.c
+++ p144.c
@@ -4,7 +4,9 @@ Concept-For Loop
 Next Program-Classify the given character is upper,lower, single digit,special character until '$'is given 
 --------------------------------------------------------------------------------------------------------------------*/
 #include<stdio.h>
+
 int main()
+
 {
 
 char cha;
@@ -13,19 +15,17 @@ for(scanf("%c",&cha); cha!='$'; scanf("%c",&cha))
 {
 
     if(cha>=65 && cha <=90)
-    printf("Upper case = %c\n",cha);
+        printf("Upper case = %c\n",cha);
     
     else if(cha>=97 && cha <=122)
-    printf("lower case = %c\n",cha);
+        printf("lower case = %c\n",cha);
     
     else if(cha>=48 && cha <=57)
-    printf("digit = %c\n",cha);
+        printf("digit = %c\n",cha);
     
     else
-    printf("special character");
+        printf("special character");
     
-
-
 
 }
 
