581bbc8fdc5a9655ce851e498192af8254429d48
Narayanmohan <mohaninbox@live.com>
2022-08-13 20:39:16 +0000

    P320-Pointers with CharArray-Read text and copy to another Array

---
+++ p320.c
@@ -0,0 +1,40 @@
+/*Program No-p320.c
+dt-8-13-2022
+Concept-Pointers with CharArray
+Next Program-Read text and copy to another Array
+--------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    char b[100];
+    char *pa,*pb;
+    
+    pa=a;
+    pb=b;
+    
+        for(pa=a;(*pa=getchar())!='$';pa++);
+                               
+                          
+             pa='\0';            
+        
+            for(pa=a;*pa!='\0';pa++)
+             {
+             
+                  *pb=*pa;
+                  
+                   pb++;                 
+      
+             }            
+            
+                   
+           
+           for(pb=b;*pb!='\0';pb++)
+            { 
+             
+               printf("%c",*pb);
+            
+            }                              
+           
+
+}
