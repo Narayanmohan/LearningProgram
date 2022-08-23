e2134c84efa0d0435aac951130495a17273ee6dd
Narayanmohan <mohaninbox@live.com>
2022-08-23 04:37:41 +0000

    P114-CharArray-whileLoop-Read txt until $ is given and copy the Last N characters

--- p114.c
+++ p114.c
@@ -2,66 +2,87 @@
 dt-8-29-2021
 Concept:Char Array
 Next Program: Read text until '$' is given and copy the last 'N' th characters 
---------------------------------------------------------------------------------------------*/
+-----------------------------------------------------------------------------------
+NOTE:
+
+ Example-Input ANAND$
+ 
+ LAST N character input ->4
+ 
+ output should be =>NAND
+ 
+cc p114.c
+./a.out p114.c
+
+ANAND$
+
+4
+
+output =>NAND
+-----------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
 
- int i,j,ctr,num;
+ int i,j,ctr,num,k;
  char a[100];
  char b[100];
  
  i=0;
- scanf("%c",&a[i]);
- 
- while(a[i]!='$')
- {
- 
-  i++;
+
   scanf("%c",&a[i]);
+ 
+  while(a[i]!='$')
+  {
+ 
+   i++;
+   
+   scanf("%c",&a[i]);
 
- }
+   }
 
-a[i]!='\0';
+a[i]='\0';
 
 i=0;
+
 ctr=0;
 
-while(a[i]!= '\0')
+while(a[i]!='\0')
 {
 
-    ctr = ctr+ i;
-    i++;
+    ctr = ctr+ 1;
 
+    i++;
 
 }
 
+k=0;
 
-
-i=0;
 j=0;
+
 scanf("%d",&num);
 
-i= num;
+k=(ctr-num);
 
-while(i<=ctr)
+while(k<ctr)
 {
 
-    b[j] = a[i];
+    b[j] = a[k];
     
-    i++;
+    k++;
     j++; 
 
 
 }
 
-b[j]!='\0';
+b[j]='\0';
 
 j=0;
 while(b[j]!='\0')
 {
 
 printf("%c",b[j]);
+
 j++;
 
 }
