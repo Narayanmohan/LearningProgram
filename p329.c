feb255f6ae0dc8e83a73ecef7aa53f438f59df76
Narayanmohan <mohaninbox@live.com>
2022-09-02 01:56:06 +0000

    P329-Pointers-Read txt until $ is given and Print the starting Address using pointers

--- p329.c
+++ p329.c
@@ -6,59 +6,57 @@ Next Program-Read text until $ is given and Print the Starting position of each 
 Char  a[100],*pa;
 char *b[100];
 
-
 NOTES:
     IF WE WANT TO ACCESS POSITION OR ADDRESS USING POINTERS THEN WE HAVE TO USE *pa 
     that means address of each character in the Array.
     
     IF WE WANT TO STORE THE POSITION OR ADDRESS TO THE ANOTHER ARRAY THEN WE HAVE TO USE
     CHAR *b[100] that means Array of address
-    
+----------------------------------------------------------------------------------------------
+When Run the Program follow the below step
+
+vagrant@vagrant:~/LearningProgram$ cc p329.c
+vagrant@vagrant:~/LearningProgram$ ./a.out p329.c
+anand
+mohan$
+
+ 0x7ffc3b7b12e0
+ 0x7ffc3b7b12e6v  
 ---------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
-
     char a[100],*pa;
     char *b[100];
-    int i;
-        
-    
-            
+    int i;              
         
             for(pa=a;(*pa=getchar())!='$';pa++);
             
-            *pa='\0';         
-               
+            *pa='\0';                       
             
                 i=0;
                 b[i]=a;
-                i++;
-            
+                i++;            
                
                 for(pa=a;*pa;pa++)
-                {
-                
+                {                
                     if(*pa=='\n')
                     {
                         
                        b[i]=pa+1;
-                       i++;                                                              
-                        
+                       i++;                                                                                      
                     
                     }
                 
-                }
-        
+                }        
                     
                     b[i]=NULL;
                     
                     for(i=0;b[i]!=NULL;i++)                   
-                    {
-                    
-                           
-                        printf("%u",b[i]);
-                        
+                    {                    
+                          
+                        printf("\n %p",b[i]);                        
                     }
+                    
                     
 }
