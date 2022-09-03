17ab93141fd803dde93c4c3f21cb696de106649e
Narayanmohan <mohaninbox@live.com>
2022-09-03 12:40:39 +0000

    P330-Pointers-Read txt until $ is given and Display the Nth line using Pointers

--- p330.c
+++ p330.c
@@ -2,10 +2,11 @@
 dt-9-1-2022
 Concept-Pointers with CharArray
 Next Program-Read text until $ is given and Display the Nth line using Pointers
--------------------------------------------------------------------------------------------------
-b[0]=address 1 should be there
-b[1]=address 2 should be there
-b[2]=address 3 should be there
+------------------------------------------------------------------------------------------------------------------
+b[0]=in b[0] should hold address of starting letter of the input word that means=> when you give sun,b[0]should have
+address of S
+b[1]=b[1] should have address of U
+b[2]=b[2] should have address of N
 
 So when we say pa = b[n-1] that means if user give 2 pa=b[2-1] ie pa=b[1]
 
@@ -25,7 +26,7 @@ Result should be moon.
 
 Address b[0] holds the address 1 but when we give value 1 from scanf "n"-> b[n-1] =>b[1-1] becomes
 0 then it will print the line in b[0].
---------------------------------------------------------------------------------------------------*/
+----------------------------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
