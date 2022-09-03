dafc4d8579dc90ccc1e5fe25f49d0cc097f457b3
Narayanmohan <mohaninbox@live.com>
2022-09-03 12:29:03 +0000

    P330-Pointers-Read txt until $ is given and Display the Nth line using Pointers

--- p330.c
+++ p330.c
@@ -2,18 +2,39 @@
 dt-9-1-2022
 Concept-Pointers with CharArray
 Next Program-Read text until $ is given and Display the Nth line using Pointers
-------------------------------------------------------------------------------------------*/
+-------------------------------------------------------------------------------------------------
+b[0]=address 1 should be there
+b[1]=address 2 should be there
+b[2]=address 3 should be there
+
+So when we say pa = b[n-1] that means if user give 2 pa=b[2-1] ie pa=b[1]
+
+when run the program
+
+cc p330.c
+./a.out p330.c
+
+sun
+moon
+wind
+$
+
+1
+
+Result should be moon.
+
+Address b[0] holds the address 1 but when we give value 1 from scanf "n"-> b[n-1] =>b[1-1] becomes
+0 then it will print the line in b[0].
+--------------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
     char a[100];
     char *pa;
     char *b[100];
-    int i,n;
+    int i,n; 
     
-
-        for(pa=a;(*pa=getchar())!='$';pa++)
-
+        for(pa=a;(*pa=getchar())!='$';pa++);
         *pa='\0';
 
         i=0;
@@ -21,26 +42,21 @@ int main()
         i++;
 
             for(pa=a;*pa;pa++)
-            {
-            
+            {            
                 if(*pa=='\n')
                 {
                 
                     b[i]=pa+1;
                     i++;
-                }
-            
-            }
+                }            
+            }           
 
-            b[i]=NULL;
+        scanf("%d",&n);  
 
-            scanf("%d",&n);
-            
-                for(i=0,pa=b[n-1];b[i]!=NULL;i++)
-                {
-                
-                    printf("\n%p",b[i]);
-                }
-
-
+        for(pa=b[n-1];*pa!='\n';pa++)
+        {
+        
+            putchar(*pa);            
+        }
+        
 }
