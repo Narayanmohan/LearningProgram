f8c54f61de868bfc48d307b89b1605271508693d
Narayanmohan <mohaninbox@live.com>
2022-09-04 02:43:36 +0000

    P331-Pointers-Read txt until $ is given and display n th line from the N th Line

--- p331.c
+++ p331.c
@@ -2,17 +2,28 @@
 dt-9-3-2022
 Concept-Pointers
 Next Program-Display nth line from mth line using Pointers
-----------------------------------------------------------------------*/
+---------------------------------------------------------------------------
+b[j]=NULL -> Since it is a Pointer we assume the Last address value is NULL
+
+if it is a char Array then last value in the array is '\0'.
+
+if it is an integer Array then the last value in the array is -1
+
+--------------------------------------------------------------------------*/
 #include<stdio.h>
 int main()
 {
-    
     char a[100];
+
     char *pa;
     char *pc;
+
     char *b[100];
+
     int i,j,n,m;
-               
+
+    m=0,n=0;           
+
            for(pa=a;(*pa=getchar())!='$';pa++);            
             
                *pa='\0';
@@ -35,20 +46,21 @@ int main()
                         }
                     }                
       
+                   b[i]=NULL;     
+      
                     scanf("%d",&n);                    
 
                     scanf("%d",&m);
                        
-                       pc=b[m];
-                                                     
-                       *pa='\0';
-                           
-                        for(pa=b[n-1];pa<pc && *pa!='\n';pa++)
+                        pc=b[m+n];
+                                  
+                                                
+                        for(pa=b[n-1];pa<pc;pa++)
                         {
                             putchar(*pa);
                         
-                        }                         
+                        }         
                                                                          
-                    
+                
 
 }
