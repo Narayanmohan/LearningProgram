2c227f2607f5a4b4fb660bc0390eeaabda7b8384
Narayanmohan <mohaninbox@live.com>
2022-09-23 01:54:51 +0000

    P350-Function-Pointers-Copy First Nth Character to Another Array Function Pointers

--- p350.c
+++ p350.c
@@ -2,19 +2,26 @@
 dt-9-22-2022
 Concept-Function-Pointers
 Next Program-Copy the First Nth Characters to Another Array using Function Pointers
+---------------------------------------------------------------------------------------------
+1-cc 350.c
+2-./a.out p350.c
+I love india$
+4
+
+O/p =>I lo
 ---------------------------------------------------------------------------------------------*/
 #include<stdio.h>
 int num;
-char a[100];
-char b[100];
 void read(char *pa);
 void firstNCha(char *pa,char *pb,int num);
 void display(char *pb);
 
 int main()
-{
-
-     
+{ 
+    char a[100];
+    
+    char b[100];    
+      
     read(a);
     
     scanf("%d",&num);
@@ -36,9 +43,9 @@ void read(char *pa)
 
 void firstNCha(char *pa, char *pb,int num)
 {
-   
+    char *pt;
 
-    for(; pa<(a+num);pa++)
+    for(pt=pa+num; pa<pt;pa++)
     {
         *pb=*pa;
          pb++;    
