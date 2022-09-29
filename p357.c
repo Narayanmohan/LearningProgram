d363ee79e2dedcc06c3e84716933cd80ce323e2d
Narayanmohan <mohaninbox@live.com>
2022-09-29 03:07:04 +0000

    P357-Function-Pointers-Read text until $ is given and Delete Comma

---
+++ p357.c
@@ -0,0 +1,68 @@
+/*Program No-P357.c
+dt-9-28-2022
+Concept-FunctionPointers
+Next Program-Read text until $ is given and Delete Comma
+----------------------------------------------------------------------*/
+#include<stdio.h>
+void read(char *pa);
+void deleteComma(char *pa, char *pb);
+void display(char *pb);
+
+int main()
+{
+    char a[100];
+    char b[100];
+    
+        read(a);
+        deleteComma(a,b);
+        display(b);
+
+
+}
+
+void read(char *pa)
+{
+
+    for(; (*pa=getchar())!='$';pa++);
+    
+    *pa='\0';
+
+
+}
+
+void deleteComma(char *pa, char *pb)
+{
+
+    for(;*pa!='\0';pa++,pb++)
+    {
+    
+        if(*pa==',')
+        {
+          pa++;
+          
+          *pb=*pa;         
+            
+        }
+        else
+        {
+        
+            *pb=*pa;
+        }        
+    
+    }
+
+*pb='\0';    
+
+}
+
+void display(char *pb)
+{
+
+    for(; *pb!='\0';pb++)
+    {
+    
+        putchar(*pb);
+    }
+
+
+}
