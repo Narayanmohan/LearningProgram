fe6e4bc85345bfcc45ff96bb2e38bdc48c1f381e
Narayanmohan <mohaninbox@live.com>
2022-10-09 04:12:30 +0000

    P360-Function-Pointers-Read txt until $ given and Display nth Line From the Mth Line

---
+++ p360.c
@@ -0,0 +1,71 @@
+/*Program No-P360.c
+dt-10-9-2022
+Concept-Function-Pointers
+Next Program-Read text until $ is given and Display nth Line from the Mth Line
+------------------------------------------------------------------------------------*/
+#include<stdio.h>
+
+void read(char *pa);
+void nthLineFromMthLine(char *pa,char **pb);
+void display(char *pa,char **pb,int m,int n);
+
+int main()
+{
+
+    int n,m;
+    
+    char a[100];
+    char *b[100];
+
+    read(a);
+    
+    scanf("%d%d",&m,&n);
+    
+    nthLineFromMthLine(a,b);
+    
+    display(a,b,m,n);   
+    
+}
+
+void read(char *pa)
+{
+    for(; (*pa=getchar())!='$';pa++);
+    *pa='\0';
+
+}
+
+void nthLineFromMthLine(char *pa,char **pb)
+{
+
+    for(; *pa;pa++)
+    {
+    
+       if(*pa=='\n')
+       {
+       
+           *pb=pa+1;
+            pb++;
+       
+       } 
+    }
+     
+*pb=NULL;
+}
+
+void display(char *pa,char **pb,int m, int n)
+{
+    
+    char *pc;
+    
+    pc=*(pb+m+n);
+    
+    for(pa=*(pb+n+1);pa<pc;pa++)
+    {
+    
+        putchar(*pa);
+    }
+
+
+}
+
+
