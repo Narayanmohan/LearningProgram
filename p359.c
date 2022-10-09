251b674765b75b653bc10e07da2983abc73344de
Narayanmohan <mohaninbox@live.com>
2022-10-09 03:54:38 +0000

    P359-Function-Pointers-Read txt until $ is given and Display the Nth Line

---
+++ p359.c
@@ -0,0 +1,63 @@
+/*Program No-P359.v
+dt-10-8-2022
+Concept-Function-Pointers
+Next Program-Read text until $ is given and Display the Nth Line
+-----------------------------------------------------------------------------------*/
+#include<stdio.h>
+void read(char *pa);
+void storeStarPos(char *pa, char **pb);
+void displayNthLine(char *pa,char **pb, int n);
+int n;
+int main()
+{
+    char a[100];
+    char *b[100];
+    
+        read(a);
+        
+        scanf("%d",&n);
+        
+        storeStarPos(a,b);
+        
+        displayNthLine(a,b,n);
+
+}
+
+void read(char *pa)
+{
+
+    for(; (*pa=getchar())!='$';pa++);
+    
+    *pa='\0';
+
+}
+
+void storeStarPos(char *pa,char **pb)
+{
+
+    for(; *pa;pa++)
+    {
+        if(*pa=='\n')
+        {
+        
+            *pb=pa+1;
+             pb++;
+        
+        }
+    
+    }
+
+*pb=NULL;
+
+}
+
+void displayNthLine(char *pa, char **pb,int n)
+{
+
+    for(pa=*(pb+(n-1)); *pa!='\n';pa++)
+    {
+        putchar(*pa);
+    
+    }
+
+}
