3a9fe4803f92e7613bb5b53ae3614ced069688fc
Narayanmohan <mohaninbox@live.com>
2022-10-16 14:54:12 +0000

    P365-Typedef-structure-Function-Area of circle using Typedef structure Function

---
+++ p365.c
@@ -0,0 +1,50 @@
+/*Program No-P365.c
+dt-10-15-2022
+Concept-TypeDef-Structure-Function
+Next Program-Area of Circle using Typedef Structure using Function
+--------------------------------------------------------------------
+
+Note:
+
+Here we can not user read function because it is pass by reference
+so we used only scanf function.
+
+-------------------------------------------------------------------*/
+#include<stdio.h>
+typedef struct areaofcircle
+{
+
+    float area;
+    int r;
+
+}aoc; 
+
+void read(aoc ao);
+
+float displayAoc(aoc ao);
+
+int main()
+{
+    aoc ao;
+
+    float ac;
+    
+    scanf("%d",&ao.r);    
+    
+    ac = displayAoc(ao);
+   
+    printf("Area of Circle=%f",ac);
+                        
+}
+   
+
+
+float displayAoc(aoc ao)
+{
+        
+    ao.area=3.14* ao.r * ao.r;    
+    
+    return ao.area;    
+    
+}
+
