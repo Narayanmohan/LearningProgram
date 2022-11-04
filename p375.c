ea7184d90bf63943e7c8ac9bfbbac4d0ece84997
Narayanmohan <mohaninbox@live.com>
2022-11-04 15:08:43 +0000

    P375-Typedef-structure-Fun.Pointers-typecast-Read biodata from file -display screen

--- p375.c
+++ p375.c
@@ -3,24 +3,41 @@ dt-11-2-2022
 Concept-Typedef Structure with Function Pointer
 Concept-Typecast
 Next Program-Read bio data from file character by character and display on the Screen
+----------------------------------------------------------------------------------------------------
+When run the file
+1-cc p375.c
+2-./a.out BIO.TXT
+
+THEN THE THE BELOW OUTPUT SHOULD BE DISPLAYED.
+
+ANAND
+ 47
+ 414744.000000
+
 ---------------------------------------------------------------------------------------------------*/
 #include<stdio.h>
+
 typedef struct biodata
-{
+ {
 
     char name[100];
+
     int age;
+
     float salary;
     
 }Bio;
 
+
 void read(FILE *fp,Bio *b);
-void display(FILE *fp,Bio *b);
+
+void display(Bio *b);
 
 int main(int argc, char *argv[])
 {
 
 Bio b;
+
 FILE *fp;
 
     char ch[sizeof(struct biodata)];
@@ -28,6 +45,7 @@ FILE *fp;
         if(argc <2)
         {
             printf("Invalid Arguments");
+            
             return 0;
         
         }
@@ -41,36 +59,36 @@ FILE *fp;
                 return 0;
                             
             }
+            
+read(fp,&b);
+
+display(&b);
 
 }
+
 
 void read(FILE *fp,Bio *b)
-{    
-    
+{   
+     int i; 
+     
+     char *px;
+     
+     px = (char *)b;
 
-    fscanf(fp,"%s%d%f",b->name,&b->age,&b->salary);
-
-}
-
-void write(FILE *fp,Bio *b)
-{
-
- int i;
- 
- char *px;
- 
- px=(char *)b;
- 
      for(i=0;i<sizeof(Bio);i++,px++)
      {
-         putc(*px,fp);
-     
+      
+       *px=getc(fp);
+       
      }
-
-
-
-
+        
+            
 }
 
-
+void display(Bio *b)
+{
+  
+     printf("%s \n %d \n %f",b->name,b->age,b->salary);  
+   
+}
 
