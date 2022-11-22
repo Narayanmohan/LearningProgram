8159eb1b7e7cd871423da5a0a8140bc3442feeaa
Narayanmohan <mohaninbox@live.com>
2022-11-22 05:02:02 +0000

    P383-Malloc-Read number until -1000 and display on the screen

--- p383.c
+++ p383.c
@@ -2,6 +2,17 @@
 dt-11-15-2022
 Concept-Malloc with Integer pointers
 Next Program-Read number until -1000 and display on the screen
+----------------------------------------------------------------
+When run the program Enter malloc size first
+
+50 - Malloc size
+1
+2
+7
+-1000
+1
+2
+7
 ---------------------------------------------------------------*/
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,30 +23,23 @@ int *pa,*pb,n;
 
 scanf("%d",&n);
 
+     pa=pb=(int *)malloc(n*sizeof(int));   
+       
 
-    pa=pb=(int *)malloc(n*sizeof(int));
-    
-    
- 
-    
-    
         for(scanf("%d",pa);*pa!=-1000;)
         {
             pa++;
                      
-            scanf("%d",pa);
-            
+            scanf("%d",pa);           
         
-        }
+        }        
         
         for(pa=pb;*pa!=-1000;pa++)
         {
-        
-            printf("%d\n",*pa);
-        
-        }  
-        
-
+                
+            printf("%d\n",*pa);        
+            
+        }      
 
 free(pb);
 
