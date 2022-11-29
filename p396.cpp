160f0482b925b8ee1f9339698f903099d995c272
Narayanmohan <mohaninbox@live.com>
2022-11-29 17:26:24 +0000

    c++-First Program-P396.cpp -Sum of Two Numbers using c++

---
+++ p396.cpp
@@ -0,0 +1,31 @@
+/*Program No-P396.c
+dt-11-28-2022
+Note -c++ 2nd Program
+Language-c++
+Next Program-Sum of Two Numbers using C++
+-------------------------------------------------
+When execute:
+
+g++ p396.cpp
+./a.out p396.cpp
+
+Then the output will be displayed on the console
+
+------------------------------------------------*/
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1,num2,result;
+    
+    cin>>num1>>num2;
+    
+    result = num1 + num2;
+    
+    
+        cout<<"Sum of Two Number is" << num1<< "+"<< num2 << "=" << result; 
+    
+
+
+}
