155d0351e7e137b5ffc78b6a6ff63d934064ba29
Narayanmohan <mohaninbox@live.com>
2022-11-29 18:30:53 +0000

    c++ -P397-Area of Circle using c++

---
+++ p397.cpp
@@ -0,0 +1,27 @@
+/*Program No-P397.cpp
+dt-11-29-2022
+Language-c++
+Next Program-Area of Circle using c++
+---------------------------------------------------
+g++ p397.cpp
+
+./a.out p397.cpp
+-------------------------------------------------*/
+#include<iostream>
+using namespace std;
+
+main()
+{
+
+    float areaofcir;
+    int r;
+    
+    cin>>r;
+    
+    areaofcir = 3.14 * r * r;
+    
+    
+        cout<<"Area of Circle =" <<areaofcir;
+        
+
+}
