89bc320c1e6cf835314c4c62450be16e6b6ed5a8
Narayanmohan <mohaninbox@live.com>
2022-11-29 17:27:57 +0000

    c++ - P395.cpp-Print the Name using c++

---
+++ p395.cpp
@@ -0,0 +1,19 @@
+/*Program No-P395.cpp
+dt-11-28-2022
+Language-C++ -1st Program
+Next Program-Print Name using c++
+------------------------------------
+g++ p395.cpp
+
+./a.out p395.cpp
+
+-----------------------------------*/
+#include<iostream>
+using namespace std;
+main()
+{
+
+    cout<<"Mohanraj Narayanaswamy";
+    
+
+} 
