23e90d56499717f59499adc1ebb5b33bb28e1b2e
Narayanmohan <mohaninbox@live.com>
2022-11-29 03:50:47 +0000

    C++ -P396-Sum of Two Number using c++

---
+++ p396.c
@@ -0,0 +1,23 @@
+/*Program No-P396.c
+dt-11-28-2022
+Note -c++ 2nd Program
+Language-c++
+Next Program-Sum of Two Numbers using C++
+------------------------------------------------*/
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1,num2,result;
+    
+    cin>>num1>>num2;
+    
+    result = num1 + num2;
+    
+    
+        cout<<Sum of Two Number is << num1 "+"<< num2 << "=" << result; 
+    
+
+
+}
