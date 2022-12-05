b74ad2b6bd85779466c6055996886559ef030a3e
Narayanmohan <mohaninbox@live.com>
2022-12-05 16:01:55 +0000

    c++-P403.cpp-Biggest of 3 numbers using Function Overloading -c++

--- p403.cpp
+++ p403.cpp
@@ -3,6 +3,16 @@ dt-12-4-2022
 Concept-Function-Overloading
 Language-c++
 Next Program-Biggest of 3 Numbers using Function Overloading-c++
+----------------------------------------------------------------------------
+Function-OverLoading
+
+Function overloading is a feature of object-oriented programming where two or more functions can have the same name but different parameters. When a function name is overloaded with different jobs it is called Function Overloading. In Function Overloading “Function” name should be the same and the arguments should be different. Function overloading can be considered as an example of a polymorphism feature in C++.
+
+The parameters should follow any one or more than one of the following conditions for Function overloading:
+
+Parameters should have a different type
+add(int a, int b)
+add(double a, double b)
 ----------------------------------------------------------------------------*/
 #include<iostream>
 using namespace std;
