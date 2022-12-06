3627fe4b57253a65e3acc0191d2c3e5dd28b3be7
Narayanmohan <mohaninbox@live.com>
2022-12-06 06:12:39 +0000

    c++-p404.cpp-FunctionOverLoading-sum of Two Numbers using Function Overloading

--- p404.cpp
+++ p404.cpp
@@ -7,17 +7,17 @@ Next Program-Sum of Two Numbers using FunctionOverLoading
 #include<iostream>
 using namespace std;
 
-void read(int *a1,int *b1,int *c1);
-void read(short *a2,short *b2, short *c2);
-void read(float *a3,float *b3,float *c3);
-void read(long *a4,long *b4,long *c4);
-void read(double *a5,double *b5,double *c5);
+void read(int *a1,int *b1);
+void read(short *a2,short *b2);
+void read(float *a3,float *b3);
+void read(long *a4,long *b4);
+void read(double *a5,double *b5);
 
-int sum2(int a1,int b1,int c1);
-short sum2(short a2,short b2,short c2);
-float sum2(float a3,float b3,float c3);
-long sum2(long a4,long b4,long c4);
-double sum2(double a5,double b5,double c5);
+int sum2(int a1,int b1);
+short sum2(short a2,short b2);
+float sum2(float a3,float b3);
+long sum2(long a4,long b4);
+double sum2(double a5,double b5);
 
 void display(int intsum2);
 void display(short shortsum2);
@@ -35,96 +35,99 @@ int main()
     double a5,b5,c5,doublesum2;
 
 
-read(&a1,&b1,&c1);
-intsum2 =sum2(a1,b1,c1);
+read(&a1,&b1);
+intsum2 =sum2(a1,b1);
 display(intsum2);
 
 
-read(&a2,&b2,&c2);
-shortsum2 =sum2(a2,b2,c2);
+read(&a2,&b2);
+shortsum2 =sum2(a2,b2);
 display(shortsum2);
 
 
-read(&a3,&b3,&c3);
-floatsum2=sum2(a3,b3,c3);
+read(&a3,&b3);
+floatsum2=sum2(a3,b3);
 display(floatsum2);
 
 
-read(&a4,&b4,&c4);
-longsum2 =sum2(a4,b4,c4);
+read(&a4,&b4);
+longsum2 =sum2(a4,b4);
 display(longsum2);
 
 
-read(&a5,&b5,&c5);
-doublesum2 =sum2(a5,b5,c5);
+read(&a5,&b5);
+doublesum2 =sum2(a5,b5);
 display(doublesum2);
 
 }
 
-void read(int *a1,int *b1,int *c1)
+void read(int *a1,int *b1)
 {
-    cin>>*a1>>*b1>>*c1;
+    cin>>*a1>>*b1;
 
 }
 
-void read(short *a2,short *b2,short *c2)
+void read(short *a2,short *b2)
 {
-    cin>>*a2>>*b2>>*c2;
+    cin>>*a2>>*b2;
 
 }
 
-void read(float *a3,float *b3,float *c3)
+void read(float *a3,float *b3)
 {
-    cin>>*a3>>*b3>>*c3;
+    cin>>*a3>>*b3;
 
 }
 
-void read(long *a4,long *b4,long *c4)
+void read(long *a4,long *b4)
 {
-    cin>>*a4>>*b4>>*c4;
+    cin>>*a4>>*b4;
 
 }
 
-void read(double *a5,double *b5,double *c5)
+void read(double *a5,double *b5)
 {
-    cin>>*a5>>*b5>>*c5;
+    cin>>*a5>>*b5;
 
 }
 
-int sum2(int a1,int b1,int c1)
+int sum2(int a1,int b1)
 {
-int s1,t1;
-s1=a1>b1?a1:b1;
-t1=s1>c1?s1:c1;
-}
-
-short sum2(short a2,short b2,short c2)
-{
-short s2,t2;
-s2=a2>b2?a2:b2;
-t2=s2>c2?s2:c2;
-}
-
-float sum2(float a3,float b3,float c3)
-{
-float s3,t3;
-s3=a3>b3?a3:b3;
-t3=s3>c3?s3:c3;
-}
-
-long sum2(long a4,long b4,long c4)
-{
-
-long s4,t4;
-s4=a4>b4?a4:b4;
-t4=s4>c4?s4:c4;
+int c1;
+c1=a1+b1;
+return c1;
 
 }
-double sum2(double a5,double b5,double c5)
+
+short sum2(short a2,short b2)
 {
-double s5,t5;
-s5=a5>b5?a5:b5;
-t5=s5>c5?s5:c5;
+short c2;
+c2=a2+b2;
+return c2;
+
+}
+
+float sum2(float a3,float b3)
+{
+float c3;
+c3=a3+b3;
+return c3;
+
+}
+
+long sum2(long a4,long b4)
+{
+
+long c4;
+c4=a4+b4;
+return c4;
+}
+double sum2(double a5,double b5)
+{
+double c5;
+c5=a5+b5;
+return c5;
+
 }
 
 void display(int intsum2)
