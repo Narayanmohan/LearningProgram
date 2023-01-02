4c1e6df232cea2ee262bcb94259d0f3d33b4f9f9
Narayanmohan <mohaninbox@live.com>
2023-01-02 20:25:35 +0000

    c++-P434-ConstructorOverLoading-Area of circle using constructorOverLoading

---
+++ p434.cpp
@@ -0,0 +1,59 @@
+/*Program No-P434.cpp
+dt-1-2-2023
+Concept-Constructor OverLoading
+Next Program-Area of Circle using Constructor OverLoading
+------------------------------------------------------------------------*/
+#include<iostream>
+using namespace std;
+class aoc
+{
+
+    private:
+        int r;
+        float result;
+
+
+    public:
+        aoc() // Defining constructor here...
+        {
+            r=7;     
+        
+        }
+        aoc(int num)
+        {
+        
+            r=num;
+        
+        }
+void findaoc();
+void display();
+};
+
+void aoc::findaoc()
+{
+    result=3.14 * r  * r;
+
+} 
+
+void aoc::display()
+{
+    cout<<result;
+
+} 
+
+int main()
+{
+
+    aoc ac;
+    ac.findaoc();
+    ac.display();
+    
+    
+        int x;
+        cin>>x;
+        
+        aoc ac2(x);
+        ac2.findaoc();
+        ac2.display();    
+    
+} 
