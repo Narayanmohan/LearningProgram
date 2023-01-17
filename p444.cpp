6bb25ba346212da92e83d2601d68cc21135df85e
Narayanmohan <mohaninbox@live.com>
2023-01-17 22:22:29 +0000

    c++-P444.cpp-MultipleInheritance-Read and Calculate and Display of payroll using MI

--- p444.cpp
+++ p444.cpp
@@ -6,4 +6,57 @@ Next Program-Read and Calculate and display of payroll process using Multiple In
 #include<iostream>
 using namespace std;
 #include "p443.cpp"
-class payroll:public 
+class payroll:public allowance,deduction
+{
+
+    public:
+        void read()
+        {
+            cout<<"Emp.Name-Age-Gen-Address"<<'\n'; 
+            cout<<"Enter Empid-Desig-Salary"<<'\n';
+        
+                allowance::read();
+                deduction::read();
+        
+        }
+
+       void display()
+       {
+            cout<<"HRA-DA-DETAILS-BELOW"<<'\n';   
+            cout<<"HRA="<<"DA";        
+        
+               allowance::display();
+               deduction::display();
+           
+       } 
+       
+       void calculate()
+       {
+       
+       cout<<"HRA-DA-ALLOWANCE CALCULATED HERE";
+       cout<<"HRA="<<"DA"<<'\n';
+     
+       cout<<"HRA-DA-ALLOWANCE CALCULATED HERE";
+       cout<<"HRA="<<"DA"<<'\n';
+
+       
+           allowance::calculate();
+           deduction::calculate();
+       
+       }
+       
+};
+
+int main()
+{
+
+    payroll obj;
+    obj.read();
+    obj.calculate();
+    obj.display();    
+
+}
+
+
+
+
