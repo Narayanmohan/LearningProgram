8708ac13760908a0c785dbf0069ea95918e1ff9b
Narayanmohan <mohaninbox@live.com>
2023-01-24 17:29:12 +0000

    c++-p446.cpp-Read a point,increment and display using operator overloading

---
+++ p446.cpp
@@ -0,0 +1,50 @@
+/*Program No-P446.cpp
+dt-1-24-2023
+Concept-Operator OverLoading
+Next Program-Read a point,increment and display using operator overloading
+-------------------------------------------------------------------------------------*/
+#include<iostream>
+using namespace std;
+class point
+{
+
+    private:
+    
+        int x,y;
+        
+    public:
+        void read();
+        void operator ++();
+        void display();
+        
+
+};
+
+void point::read()
+{
+    cin>>x>>y;
+}
+
+void point::opeartor ++()
+{
+
+    x++;
+    y++;
+
+}
+
+void point::display()
+{
+    cout<<x<<y;
+
+}
+
+int main()
+{
+
+    point p;
+    p.read();
+    p++;
+    p.display();
+    
+}
