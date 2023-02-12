c9c4f0773a0e3376c040e4efe02de1babf44c037
Narayanmohan <mohaninbox@live.com>
2023-02-12 21:32:40 +0000

    c++-p463.cpp-Read biodata from keyboard and write it to file using class

---
+++ p463.cpp
@@ -0,0 +1,56 @@
+/*Program No-P463.cpp
+dt-2-12-2023
+Concept-FileHandling with Class
+Next Program-Read biodata from keyboard and write it to file using class
+---------------------------------------------------------------------------------*/
+#include<fstream>
+#include<iostream>
+class person
+{
+    private:
+        char name[100];
+        int age;
+        float salary;
+
+
+    public:
+        void read();       
+
+};
+
+
+void person::read()
+{
+
+    cin>>name>>age>>salary;
+
+}
+
+int main(int argc, char *argv[])
+{
+
+    ofstream bio(argv[1]);
+    
+        if(argc<2)
+        {
+            cout<<"Invalid Argument";
+            return 0;
+                       
+        }
+        
+        if(bio.fail())
+        {
+            cout<<"File Opening Error";
+            return 0;
+        
+        }
+       
+    
+        person p;
+        p.read();
+        
+        bio.write((char *)&p,sizeof(p));
+        
+        bio.close();
+   
+}
