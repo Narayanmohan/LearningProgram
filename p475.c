1ae9a90bbb1669faaee500d40c897bfd030eee8d
Narayanmohan <mohaninbox@live.com>
2023-02-15 19:18:31 +0000

    Debugging-Day01-05

---
+++ p475.c
@@ -0,0 +1,13 @@
+/*Program No-P475.c
+dt-2-15-2023
+Concept-Debugging
+Next Program-Debugging-Day01-05
+-----------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    printf("%ld %ld", sizeof('2'),sizeof("2")); 
+
+}
+
