b19179ac25468897d6b07bb0aaf639afef7712ab
Narayanmohan <mohaninbox@live.com>
2023-02-15 19:16:35 +0000

    Debugging-Day01-04

---
+++ p472.c
@@ -0,0 +1,11 @@
+/*Program No-P472.c
+dt-2-15-2023
+Concept-Debugging
+Next Program-Debugging-Day01-04
+-----------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    printf("%ld %ld",sizeof(2),sizeof(2.0));
+    
+}
