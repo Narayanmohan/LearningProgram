b320624c862d48e67b959658268780a2996fe870
Narayanmohan <mohaninbox@live.com>
2023-02-15 18:58:58 +0000

    Debugging-Day01-01

---
+++ p469.c
@@ -0,0 +1,15 @@
+/*Program No-P469.c
+Dt-02-15-2023
+Concept-Debugging in printf
+Day-1
+-------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    printf("Welcome to Griffin");
+
+}
+
+
+   
