60eae709761b28a0505e5309ffbc908dd0f606d8
Narayanmohan <mohaninbox@live.com>
2023-02-15 19:07:16 +0000

    Debugging-Day01-2

---
+++ p470.c
@@ -0,0 +1,11 @@
+/*Program No-P470.c
+dt-2-15-2023
+Concept-Debugging
+Next Program-Day01-2Program
+--------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    printf("%ld %ld %ld",sizeof(int),sizeof(long),sizeof(short));
+
+}
