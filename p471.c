2156aff677dfdb733ca15b33b1a740451237cfda
Narayanmohan <mohaninbox@live.com>
2023-02-15 19:11:09 +0000

    Debugging-Day01-03

---
+++ p471.c
@@ -0,0 +1,12 @@
+/*Program No-P471.c
+dt-2-15-2023
+Concept-Debugging
+Next Program-Debugging-Day01-03
+-------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    printf("%ld %ld %ld",sizeof(float),sizeof(double),sizeof(char)); 	
+
+}
