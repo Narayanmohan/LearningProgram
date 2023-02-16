714632ae3ef0b59f6596a27a59331b7134e4b217
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:46:48 +0000

    Debugging-Day02-09

---
+++ p477.c
@@ -0,0 +1,14 @@
+/*
+Program No-P477.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day02-09
+-------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+        int z; z=10+010+0x101; printf("%d",z);
+
+}
+
