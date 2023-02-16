09f3bdd0e109090caad2a9c4076a117967cdbdc8
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:24:15 +0000

    Debugging-Day03-13

---
+++ p481.c
@@ -0,0 +1,21 @@
+/*
+Program No-P481.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day03-13
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int a,b,c;
+    
+    scanf("%d %d",a,b); 
+    
+    c=a+b; 
+    
+            printf("%d",c); //(inputs are 5 and 6 )
+    
+}
+
+
