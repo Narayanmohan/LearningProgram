d53144e8f12600393724d4027a410e34d329e3c9
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:41:55 +0000

    Debugging-Day04-20

---
+++ p488.c
@@ -0,0 +1,14 @@
+/*
+Program No-P488.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day04-20
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int xx0xx1xx2xx3xx4xx5xx6xx7xx8xx9xxzz1zz2=100;
+printf("%d",xx0xx1xx2xx3xx4xx5xx6xx7xx8xx9xxzz1z); 
+
+}
