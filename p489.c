f589d7c44e9fc4b36f089802f0d7e7e1a8a68e1f
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:42:56 +0000

    Debugging-Day05-21

---
+++ p489.c
@@ -0,0 +1,12 @@
+/*
+Program No-P489.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day05-21
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+     int x,y; x=10.1111; y=10.000001; printf("%d %d",x,y); 
+
+}
