03a0c3a239fe75b9bec0d0a7aba1de9cdb30a122
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:56:39 +0000

    Debugging-Day03-11

---
+++ p479.c
@@ -0,0 +1,13 @@
+/*
+Program No-P479.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day03-11
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    
+    int a; a=6%7; printf("%d",a);
+
+}
