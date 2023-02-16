d179d019d1b40ecfab76a59d16bb1254e6149bb4
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:49:37 +0000

    Debugging-Day02-10

---
+++ p478.c
@@ -0,0 +1,13 @@
+/*
+Program No-P478.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day02-10
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int z; float x,y; x=10.5; y=10.0; z=x+y; printf("%d",z); 
+
+}
