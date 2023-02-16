37c6086c203160c601fef620159565499a6323fa
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:26:04 +0000

    Debugging-Day03-14

---
+++ p482.c
@@ -0,0 +1,13 @@
+/*
+Program No-P482.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day03-14
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int x; x=9%11 + 11%10; printf("x= %d",x);
+
+}
