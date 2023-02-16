327efabcd1e3f3fdb30547c03649c37c9460eaff
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:39:08 +0000

    Debugging-Day04-18

---
+++ p486.c
@@ -0,0 +1,13 @@
+/*
+Program No-P486.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day04-18
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int x,y; x=-5 - -5; y=-5 - -(-5); printf("x=%d y=%d",x,y);
+
+}
