d90418ec28200d99766dd872d1767e3b0ac33b7c
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:30:38 +0000

    Debugging-Day02-06

---
+++ p473.c
@@ -0,0 +1,12 @@
+/*Program No-P473.c
+dt-2-15-2023
+Concept-Debugging-Day-02
+Next Program-Day02-01
+----------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    printf("%d %d",sizeof(2l),sizeof(2.0f));
+
+}
+
