615c26ddc29c9c8e6dbf9ef32d59aed84134e69d
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:35:59 +0000

    Debugging-Day04-16

---
+++ p484.c
@@ -0,0 +1,14 @@
+/*
+Program No-P484.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day04-16
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int i=10,j=20;
+         printf("%d %d");
+
+}
