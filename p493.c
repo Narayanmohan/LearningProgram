ad7f549192b2af402c46ba2c62ed465bf5da6bad
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:47:24 +0000

    Debugging-Day05-25

---
+++ p493.c
@@ -0,0 +1,14 @@
+/*
+Program No-P493.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day05-25
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int a=65; printf("%d %o %x",a,a,a);
+
+}
+
