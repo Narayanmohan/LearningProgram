29a2cf80e73d648e92bbdc4e605837a1793ec4c5
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:37:42 +0000

    Debugging-Day04-17

---
+++ p485.c
@@ -0,0 +1,12 @@
+/*
+Program No-P485.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day04-16
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+     int x,y=20; printf("%d %d",x,y);
+}
