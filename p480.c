6f2dd709fc4ff07f8aa9777014fec132fe152931
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:58:13 +0000

    Debugging-Day03-12

---
+++ p480.c
@@ -0,0 +1,13 @@
+/*
+Program No-P480.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day03-12
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+    
+        int a,b,c; b=6; c=7; a=b/c; printf("%d",a); 
+
+}
