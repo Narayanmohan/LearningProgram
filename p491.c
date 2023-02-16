74b387d73c328167bb748ab2b74d7b3037ad60d3
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:45:11 +0000

    Debugging-Day05-23

---
+++ p491.c
@@ -0,0 +1,13 @@
+/*
+Program No-P491.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day05-23
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int x; x=2**3-2^3; printf("%d",x);
+
+}
