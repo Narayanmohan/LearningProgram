f747d3191f252fd2de7c3ceb0e7a2c18b545d985
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:43:58 +0000

    Debugging-Day02-08

---
+++ p476.c
@@ -0,0 +1,14 @@
+/*
+Program No-P476.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day02-03
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int x; x=010+10+0101; printf("%d",x);
+
+}
+
