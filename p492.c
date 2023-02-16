7c85ce707bb81ac92575a3cf995df14009722ee9
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:46:27 +0000

    Debugging-Day05-24

---
+++ p492.c
@@ -0,0 +1,13 @@
+/*
+Program No-P492.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day05-24
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+     int x=4;y=5,z; z=x*y/4-15/15+18/3*20/y; printf("%d",z); 
+
+}
