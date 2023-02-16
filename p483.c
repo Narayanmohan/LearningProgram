bb5ad21b43971e33c5f5dd220640eb06500ce9e4
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:28:04 +0000

    Debugging-Day03-14

---
+++ p483.c
@@ -0,0 +1,13 @@
+/*
+Program No-P483.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day03-14
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    int i; float j; i=5/2; j=5/2; printf("%d %f",i,j);
+
+}
