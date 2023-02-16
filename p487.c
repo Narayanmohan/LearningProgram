751f953538fb31b9b37245d444952a2fbf79b88b
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:40:29 +0000

    Debugging-Day04-19

---
+++ p487.c
@@ -0,0 +1,13 @@
+/*
+Program No-P487.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day04-19
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+     printf("G\nr\ni\nf\rfin");
+
+}
