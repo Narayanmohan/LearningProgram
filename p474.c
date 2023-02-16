770552d7fc2f40eeff73eaeac11bd34ffeb557cd
Narayanmohan <mohaninbox@live.com>
2023-02-16 02:38:25 +0000

    Debugging-Day02-02

---
+++ p474.c
@@ -0,0 +1,17 @@
+/*Program No-P474.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day02-02
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    float area,b,h;
+        b=20;
+        h=10; 
+    
+            area=1/2*b*h;    
+            printf("%f",area); 	
+    
+}
