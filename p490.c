ec14b074fe7bc39835a7181a8ed5114cf6caf9e8
Narayanmohan <mohaninbox@live.com>
2023-02-16 03:44:08 +0000

    Debugging-Day05-22

---
+++ p490.c
@@ -0,0 +1,13 @@
+/*
+Program No-P490.c
+dt-2-15-2023
+Concept-Debugging
+Next program-Debugging-Day05-22
+----------------------------------------------------------------------*/
+#include<stdio.h>
+int main()
+{
+
+    long x; x=65536+300; printf("%d %ld",x,x);
+
+}
